a3f65104f3035a8bdd892be1bbde33bf1c9af652
TamyCristina <112989809+TamyCristina@users.noreply.github.com>
2022-10-20 14:52:24 -0300

    exercicio3

---
+++ exercicio3.js
@@ -0,0 +1,8 @@
+let valorProduto1=15
+let valorProduto2=30
+let lucro1="0.45"
+let lucro2="0.30"
+let valorFinalDoProduto1=valorProduto1*lucro1
+console.log(valorFinalDoProduto1)
+let valorFinalDoProduto2=valorProduto2*lucro2
+console.log(valorFinalDoProduto2)
